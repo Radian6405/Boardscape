161d68709fe1072c85a21666df443fd69b885c4b
Sai Vardhan <vardhan.6405@gmail.com>
2024-07-08 20:47:06 +0530

    build: postgreSQL connected with server --environment variables added with dotenv --docker file updated --npm scripts updated --Readme added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,19 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
+import pool from "./db.js";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const app = express();
+const app: Express = express();
 
-const PORT = 8000;
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+const PORT: number = Number(process.env.SERVER_PORT ?? 8000);
+
+app.get("/", async (req: Request, res: Response) => {
+  res.status(200).send("hello");
+});
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
