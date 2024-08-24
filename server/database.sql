9ef018a370d99743b7961b6eec373444f8679275
Sai Vardhan <vardhan.6405@gmail.com>
2024-08-24 16:07:36 +0530

    feat: database to store active room's boards

--- server/database.sql
+++ server/database.sql
@@ -13,3 +13,9 @@ CREATE TABLE room_data (
     is_game_started BOOLEAN NOT NULL,
     game VARCHAR(24) NOT NULL
 );
+
+CREATE TABLE tictactoe_board_data (
+    board_id SERIAL NOT NULL PRIMARY KEY,
+    room_id INTEGER REFERENCES room_data ON DELETE CASCADE NOT NULL ,
+    board BOOLEAN[][]
+);
