116c7f589813bac79719e4b9df4e92c42d8aefa0
Sai Vardhan <vardhan.6405@gmail.com>
2024-09-24 19:32:30 +0530

    fix: responsive room page

--- client/src/components/util/game_cards/Misc.tsx
+++ client/src/components/util/game_cards/Misc.tsx
@@ -7,21 +7,21 @@ export function RoomCodeCard({ code }: { code: string }) {
   return (
     <>
       <div
-        className="flex h-1/4 flex-col items-center justify-center gap-4 rounded-xl border-4 border-dashed
-                  border-dark-primary bg-background/50 px-10"
+        className="flex flex-col items-center justify-center gap-4 rounded-xl border-4 border-dashed
+                  border-dark-primary bg-background/50 py-2"
       >
-        <div className="font-nueu text-4xl font-extrabold text-accent">
+        <div className="font-nueu text-3xl font-extrabold text-accent lg:text-4xl">
           Room Code:
         </div>
-        <div className="flex flex-row items-center justify-center gap-6">
+        <div className="flex flex-row items-center justify-center gap-2 lg:gap-4">
           <div className="flex flex-col gap-1">
             <div
               className="flex items-center justify-center rounded-lg border-2 border-primary bg-background 
-            px-6 py-3 font-nueu text-3xl font-bold text-text"
+            px-6 py-3 font-nueu text-2xl font-bold text-text lg:text-3xl"
             >
               {code}
             </div>
-            <div className="text-center text-sm text-text/50">
+            <div className="text-center text-xs text-text/50 md:text-sm">
               Use code to invite
             </div>
           </div>
@@ -41,12 +41,14 @@ export function RoomCodeCard({ code }: { code: string }) {
               }}
             >
               {copyDebug ? (
-                <IconCheckbox className="size-8" stroke={2} />
+                <IconCheckbox className="size-6 lg:size-8" stroke={2} />
               ) : (
-                <IconCopy className="size-8" stroke={2} />
+                <IconCopy className="size-6 lg:size-8" stroke={2} />
               )}
             </div>
-            <div className="text-center text-sm text-text/50">Copy link</div>
+            <div className="text-center text-xs text-text/50 md:text-sm">
+              Copy link
+            </div>
           </div>
         </div>
       </div>
@@ -77,7 +79,9 @@ export function Chat({
 
   return (
     <>
-      <div className="font-nueu text-4xl font-extrabold text-accent">Chat</div>
+      <div className="py-2 font-nueu text-4xl font-extrabold text-accent md:text-5xl">
+        Chat
+      </div>
       <div className="h-full w-full overflow-y-auto px-2">
         {chatList.map((entry, i) => {
           return (
@@ -97,10 +101,10 @@ export function Chat({
           );
         })}
       </div>
-      <div className="flex flex-row gap-4 px-4">
+      <div className="flex w-full flex-row">
         <input
           type="text"
-          className="h-14 w-60 border-b-2 border-accent bg-transparent p-2 text-xl text-text
+          className="h-14 w-4/5 border-b-2 border-primary bg-transparent p-2 text-xl text-text
           focus:outline-0"
           placeholder="send message"
           value={message ?? ""}
@@ -112,10 +116,10 @@ export function Chat({
           }}
         />
         <div
-          className="flex cursor-pointer items-center justify-center rounded-lg bg-primary px-4 py-2 text-white hover:bg-accent"
+          className="flex cursor-pointer items-center justify-center rounded-r-lg bg-primary px-4 text-white hover:bg-accent"
           onClick={sendMessage}
         >
-          <IconSend2 stroke={2} className="size-9" />
+          <IconSend2 stroke={2} className="size-6 md:size-8" />
         </div>
       </div>
     </>
